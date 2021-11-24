77a5f84f52d9fdc2cb140b0ec1b2f5df7bf09c67
Light Bringer <rfarley8@outlook.com>
2021-11-24 15:35:51 -0500

    new updates

--- app/src/main/java/com/example/basic_phrases/MainActivity.java
+++ app/src/main/java/com/example/basic_phrases/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,19 +17,120 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void buttonTapped(View view) {
+    public void hello(View view) {
 
-        int id = view.getId();
-        String ourId;
+        bt = findViewById(R.id.button1);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.hello);
+        bt.setOnClickListener(v -> mp.start());
 
-        ourId = view.getResources().getResourceEntryName(id);
-
-        int resourceId = getResources().getIdentifier(ourId, "raw", "com.example.basic_phrases");
-
-        MediaPlayer mplayer = MediaPlayer.create(this, resourceId);
-        mplayer.start();
-
-        Log.i("button tapped", ourId);
     }
 
+    public void goodAfternoon(View view) {
+
+        bt = findViewById(R.id.button2);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.good_afternoon);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void goodEvening(View view) {
+
+        bt = findViewById(R.id.button3);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.good_evening);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void hiBye(View view) {
+
+        bt = findViewById(R.id.button4);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.hi_bye);
+        bt.setOnClickListener(v -> mp.start());
+    }
+
+    public void goodbye(View view) {
+
+        bt = findViewById(R.id.button5);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.goodbye);
+        bt.setOnClickListener(v -> mp.start());
+    }
+
+    public void please(View view) {
+
+        bt = findViewById(R.id.button6);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.please);
+        bt.setOnClickListener(v -> mp.start());
+    }
+
+    public void seeYou(View view) {
+
+        bt = findViewById(R.id.button7);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.seeyou);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void seeYouLater(View view) {
+
+        bt = findViewById(R.id.button8);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.seeyou_later);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void seeYouSoon(View view) {
+
+        bt = findViewById(R.id.button9);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.seeyou_soon);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void seeYouTomorrow(View view) {
+
+        bt = findViewById(R.id.button10);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.seeyou_tomorrow);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void thankYou(View view) {
+
+        bt = findViewById(R.id.button11);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.thankyou_verymuch);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void youreWelcome(View view) {
+
+        bt = findViewById(R.id.button12);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.youre_welcome);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void welcome(View view) {
+
+        bt = findViewById(R.id.button13);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.welcome);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void imSorry(View view) {
+
+        bt = findViewById(R.id.button14);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.im_sorry);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
+
+    public void excuseMe(View view) {
+
+        bt = findViewById(R.id.button15);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.excuse_me);
+        bt.setOnClickListener(v -> mp.start());
+
+    }
 }
